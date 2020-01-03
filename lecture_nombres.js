9c8bd169d6031966678827bcc256ffe2745c4890
Petitboy <49115857+Petitboy@users.noreply.github.com>
2020-01-03 16:11:15 +0100

    Add files via upload

--- lecture_nombres.js
+++ lecture_nombres.js
@@ -2,9 +2,6 @@
  *  VARIABLES *
  **************/
 
-const affichageUnites = document.getElementById('affichage-unites');
-const affichageDizaines = document.getElementById('affichage-dizaines');
-const affichageCentaines = document.getElementById('affichage-centaines');
 const affichageMelange = document.getElementById('affichage-melange');
 const titreNombres = document.getElementById('titre-nombres');
 
@@ -15,6 +12,9 @@ const titreNombres = document.getElementById('titre-nombres');
 var unites = [];
 var dizaines = [];
 var centaines = [];
+var unitesMille = [];
+var dizainesMille = [];
+var centainesMille = [];
 
 var baseUnites = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var baseIdUnites = ['zero', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf'];
@@ -22,6 +22,12 @@ var baseDizaines = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var baseIdDizaines = ['zero-dizaines', 'dix', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante-dix', 'quatre-vingts', 'quatre-vingt-dix'];
 var baseCentaines = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var baseIdCentaines = ['zero-centaines', 'cent', 'deux-cents', 'trois-cents', 'quatre-cents', 'cinq-cents', 'six-cents', 'sept-cents', 'huit-cents', 'neuf-cents'];
+var baseUnitesMille = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var baseIdUnitesMille = ['zero-mille', 'un-mille', 'deux-mille', 'trois-mille', 'quatre-mille', 'cinq-mille', 'six-mille', 'sept-mille', 'huit-mille', 'neuf-mille'];
+var baseDizainesMille = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var baseIdDizainesMille = ['zero-dix-mille', 'un-dix-mille', 'deux-dix-mille', 'trois-dix-mille', 'quatre-dix-mille', 'cinq-dix-mille', 'six-dix-mille', 'sept-dix-mille', 'huit-dix-mille', 'neuf-dix-mille'];
+var baseCentainesMille = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var baseIdCentainesMille = ['zero-cent-mille', 'un-cent-mille', 'deux-cent-mille', 'trois-cent-mille', 'quatre-cent-mille', 'cinq-cent-mille', 'six-cent-mille', 'sept-cent-mille', 'huit-cent-mille', 'neuf-cent-mille'];
 
 /***************************************
 * FONCTION DE CREATION DE CONTENU HTML *
@@ -42,7 +48,9 @@ function createNewInput (baseId, base, letterRow) {
 createNewInput(baseIdUnites, baseUnites, 'form-group-unites');
 createNewInput(baseIdDizaines, baseDizaines, 'form-group-dizaines');
 createNewInput(baseIdCentaines, baseCentaines, 'form-group-centaines');
-
+createNewInput(baseIdUnitesMille, baseUnitesMille, 'form-group-unites-mille');
+createNewInput(baseIdDizainesMille, baseDizainesMille, 'form-group-dizaines-mille');
+createNewInput(baseIdCentainesMille, baseCentainesMille, 'form-group-centaines-mille');
 
 /*********************************************
  *  FONCTION DE SELECTION DES CASES À COCHER *
@@ -53,18 +61,19 @@ function select (base, baseId, unit, affichage) {
 		const x = document.getElementById(baseId[i]).addEventListener('change', ($event) => {
 			if ($event.target.checked) {
 				unit.push(base[i]);
-				affichage.textContent = unit;
 			} else {
 				const index = unit.indexOf(base[i]);
-				unit.splice(index, 1);
-				affichage.textContent = unit;}
+				unit.splice(index, 1);}
 		});
 	}
 }
 
-select(baseUnites, baseIdUnites, unites, affichageUnites);
-select(baseDizaines, baseIdDizaines, dizaines, affichageDizaines);
-select(baseCentaines, baseIdCentaines, centaines, affichageCentaines);
+select(baseUnites, baseIdUnites, unites);
+select(baseDizaines, baseIdDizaines, dizaines);
+select(baseCentaines, baseIdCentaines, centaines);
+select(baseUnitesMille, baseIdUnitesMille, unitesMille);
+select(baseDizainesMille, baseIdDizainesMille, dizainesMille);
+select(baseCentainesMille, baseIdCentainesMille, centainesMille);
 
 /************************
  *  FONCTION DE MÉLANGE *
@@ -72,7 +81,6 @@ select(baseCentaines, baseIdCentaines, centaines, affichageCentaines);
 
 function melangerUnChiffre(donnees, affichage) {		
 	affichageMelange.textContent = " ";
-	titreNombres.textContent = "Lire les nombres suivants : ";	
 	if (donnees.length == 0) {
 		window.alert("Vous devez choisir au moins un chiffre des unités !");}
 		else {
@@ -84,7 +92,6 @@ function melangerUnChiffre(donnees, affichage) {
 
 function melangerDeuxChiffres(donneesUnites, donneesDizaines, affichage) {		
 	affichageMelange.textContent = " ";
-	titreNombres.textContent = "Lire les nombres suivants : ";	
 	if ((donneesUnites.length == 0) || (donneesDizaines.length == 0)){
 		window.alert("Vous devez choisir au moins un chiffre des unités et un chiffre des dizaines !");}
 		else {
@@ -97,7 +104,6 @@ function melangerDeuxChiffres(donneesUnites, donneesDizaines, affichage) {
 
 function melangerTroisChiffres(donneesUnites, donneesDizaines, donneesCentaines, affichage) {		
 	affichageMelange.textContent = " ";
-	titreNombres.textContent = "Lire les nombres suivants : ";	
 	if ((donneesUnites.length == 0) || (donneesDizaines.length == 0) || (donneesCentaines.length == 0)){
 		window.alert("Vous devez choisir au moins un chiffre des unités, un chiffre des dizaines et un chiffre des centaines !");}
 		else {
@@ -111,6 +117,57 @@ function melangerTroisChiffres(donneesUnites, donneesDizaines, donneesCentaines,
 	}	
 }
 
+function melangerQuatreChiffres(donneesUnites, donneesDizaines, donneesCentaines, donneesUnitesMille, affichage) {		
+	affichageMelange.textContent = " ";
+	if ((donneesUnites.length == 0) || (donneesDizaines.length == 0) || (donneesCentaines.length == 0) || (donneesUnitesMille.length == 0)){
+		window.alert("Vous devez choisir au moins un chiffre des unités, des dizaines, des centaines et des unités de mille !");}
+		else {
+	for (compteur=0; compteur <60; compteur +=1) {
+		var nombreAleatoireUnites = Math.floor(Math.random() * donneesUnites.length);
+		var nombreAleatoireDizaines = Math.floor(Math.random() * donneesDizaines.length);
+		var nombreAleatoireCentaines = Math.floor(Math.random() * donneesCentaines.length);
+		var nombreAleatoireUnitesMille = Math.floor(Math.random() * donneesUnitesMille.length);
+		affichageMelange.textContent += donneesUnitesMille[nombreAleatoireUnitesMille] + donneesCentaines[nombreAleatoireCentaines] + donneesDizaines[nombreAleatoireDizaines] 
+		+ donneesUnites[nombreAleatoireUnites] + " ";
+		}
+	}	
+}
+
+function melangerCinqChiffres(donneesUnites, donneesDizaines, donneesCentaines, donneesUnitesMille, donneesDizainesMille, affichage) {		
+	affichageMelange.textContent = " ";
+	if ((donneesUnites.length == 0) || (donneesDizaines.length == 0) || (donneesCentaines.length == 0) || (donneesUnitesMille.length == 0) || (donneesDizainesMille.length == 0)){
+		window.alert("Vous devez choisir au moins un chiffre des unités, des dizaines, des centaines, des unités de mille et des dizaines de mille !");}
+		else {
+	for (compteur=0; compteur <60; compteur +=1) {
+		var nombreAleatoireUnites = Math.floor(Math.random() * donneesUnites.length);
+		var nombreAleatoireDizaines = Math.floor(Math.random() * donneesDizaines.length);
+		var nombreAleatoireCentaines = Math.floor(Math.random() * donneesCentaines.length);
+		var nombreAleatoireUnitesMille = Math.floor(Math.random() * donneesUnitesMille.length);
+		var nombreAleatoireDizainesMille = Math.floor(Math.random() * donneesDizainesMille.length);
+		affichageMelange.textContent += donneesDizainesMille[nombreAleatoireDizainesMille] + donneesUnitesMille[nombreAleatoireUnitesMille] + donneesCentaines[nombreAleatoireCentaines] + donneesDizaines[nombreAleatoireDizaines] 
+		+ donneesUnites[nombreAleatoireUnites] + " ";
+		}
+	}	
+}
+
+function melangerSixChiffres(donneesUnites, donneesDizaines, donneesCentaines, donneesUnitesMille, donneesDizainesMille, donneesCentainesMille, affichage) {		
+	affichageMelange.textContent = " ";
+	if ((donneesUnites.length == 0) || (donneesDizaines.length == 0) || (donneesCentaines.length == 0) || (donneesUnitesMille.length == 0) || (donneesDizainesMille.length == 0) || (donneesCentainesMille.length == 0)){
+		window.alert("Vous devez choisir au moins un chiffre des unités, des dizaines, des centaines, des unités de mille, des dizaines de mille et des centaines de mille !");}
+		else {
+	for (compteur=0; compteur <60; compteur +=1) {
+		var nombreAleatoireUnites = Math.floor(Math.random() * donneesUnites.length);
+		var nombreAleatoireDizaines = Math.floor(Math.random() * donneesDizaines.length);
+		var nombreAleatoireCentaines = Math.floor(Math.random() * donneesCentaines.length);
+		var nombreAleatoireUnitesMille = Math.floor(Math.random() * donneesUnitesMille.length);
+		var nombreAleatoireDizainesMille = Math.floor(Math.random() * donneesDizainesMille.length);
+		var nombreAleatoireCentainesMille = Math.floor(Math.random() * donneesCentainesMille.length);
+		affichageMelange.textContent += donneesCentainesMille[nombreAleatoireCentainesMille] + donneesDizainesMille[nombreAleatoireDizainesMille] + donneesUnitesMille[nombreAleatoireUnitesMille] + donneesCentaines[nombreAleatoireCentaines] + donneesDizaines[nombreAleatoireDizaines] 
+		+ donneesUnites[nombreAleatoireUnites] + " ";
+		}
+	}	
+}
+
 /****************************
  *  FONCTION DE MISE À ZÉRO *
  ****************************/
@@ -118,13 +175,13 @@ function melangerTroisChiffres(donneesUnites, donneesDizaines, donneesCentaines,
 function reset() {
 	var clist = document.getElementsByTagName("input");
 	for (var i = 0; i < clist.length; ++i) { clist[i].checked = false; }
-	affichageCentaines.textContent = " ";
-	affichageDizaines.textContent = " ";
-	affichageUnites.textContent = " ";
 	affichageMelange.textContent = " ";
 	unites.splice(0, unites.length);
 	dizaines.splice(0, dizaines.length);
 	centaines.splice(0, centaines.length);
+	unitesMille.splice(0, unitesMille.length);
+	dizainesMille.splice(0, dizainesMille.length);
+	centainesMille.splice(0, centainesMille.length);
 }
 
 
